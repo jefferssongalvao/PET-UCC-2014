a0e911e981c633b9ac1740c13817986e5eed5c4c
Jeffersson Galvão <jeffersson.webdeveloper@gmail.com>
2014-04-14 14:56:31 -0300

    Questão G da fase de grupos

--- grupo/questao_d.cpp
+++ grupo/questao_d.cpp
@@ -6,7 +6,7 @@ using namespace std;
 int main(){
 	string v[12] = { "C", "C#/Db", "D", "D#/Eb", "E", "F", "F#/Gb", "G", "G#/Ab", "A", "A#/Bb","B" };
 	int tecla;
-	while(cin>>tecla ){
+	while(cin >> tecla ){
 		cout << v[tecla % 12] << endl;
 	}
 }
---
+++ grupo/questao_g.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+
+using std::cout;
+using std::cin;
+using std::endl;
+
+void torre (int n, char orig, char dest, char aux) {
+	if (n == 1) {
+		cout << n << "-" << orig << "-" << dest << endl;
+		return;
+	}
+	torre(n-1,orig,aux,dest);
+	cout << n << "-" << orig << "-" << dest << endl;
+	torre(n-1,aux,dest,orig);
+}
+int main() {
+	int discos;
+	while(cin >> discos){
+		torre(discos, 'a', 'c', 'b');
+	}
+	return 0;
+}
